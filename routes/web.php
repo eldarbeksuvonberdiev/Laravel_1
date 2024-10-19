7e22df401bfe339dd91a786e97dd5a7144be9c1d
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-10-19 08:33:55 +0500

    Pagelar almashtirildi

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,38 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/index1');
 });
+Route::get('/ind2', function () {
+    return view('pages/index2');
+});
+Route::get('/ind3', function () {
+    return view('pages/index3');
+});
+Route::get('/widget', function () {
+    return view('pages/widget');
+});
+Route::get('/layout1', function () {
+    return view('layouts/layout1');
+});
+// Route::get('/', function () {
+//     return view('main');
+// });
+// Route::get('/', function () {
+//     return view('main');
+// });
+// Route::get('/', function () {
+//     return view('main');
+// });
+// Route::get('/', function () {
+//     return view('main');
+// });
+// Route::get('/', function () {
+//     return view('main');
+// });
+// Route::get('/', function () {
+//     return view('main');
+// });
+// Route::get('/', function () {
+//     return view('main');
+// });
